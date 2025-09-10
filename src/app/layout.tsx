8d902e5602600b34418dcccf7d41e7f0c3a3195c
wal-mariana-laura <>
2025-09-10 15:57:13 -0300

    feat: apply dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      className="dark"
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
